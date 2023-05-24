7cafc17bdf3137b1d494207b0e8325b2ed10c2e2
aysebat <aaysebatt@gmail.com>
2023-05-24 22:45:44 +0300

    signal and slot label and input edit example

---
+++ signals_slots_4.py
@@ -0,0 +1,29 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QLabel, QLineEdit
+from PyQt6.QtWidgets import QVBoxLayout, QWidget
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        self.label = QLabel()
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
